6d73a793b17c2ef3175a74404f4cf545a63d968d
elliottnes <60632551+elliottnes@users.noreply.github.com>
2020-10-22 09:26:29 -0700

    QA

---
+++ js/nav.js
@@ -0,0 +1,30 @@
+/* Open */
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+/* Open */
+function openNav1() {
+  document.getElementById("myNav1").style.height = "100%";
+}
+
+/* Close */
+function closeNav1() {
+  document.getElementById("myNav1").style.height = "0%";
+}
+
+
+/* Open the sidenav */
+function openNav3() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav3() {
+  document.getElementById("mySidenav").style.width = "0";
+}
