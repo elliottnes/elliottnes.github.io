6868cae2a79e81217357d75e0b715b819547b2e3
elliottnes <60632551+elliottnes@users.noreply.github.com>
2020-11-04 12:10:51 -0800

    Clean Up / Bug Fixes

--- js/nav.js
+++ js/nav.js
@@ -28,3 +28,18 @@ function openNav3() {
 function closeNav3() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  m = checkTime(m);
+  document.getElementById('txt').innerHTML =
+  h + ":" + m + " PST";
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
